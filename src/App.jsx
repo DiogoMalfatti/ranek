fab9720c2e3140c58a6fced8f2fdaf55aace135d
Diogo Malfatti <diimalfatti@gmail.com>
2025-01-11 15:36:42 -0300

    create route, component produto and comonent produtos

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Produtos from "./components/Produtos";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Contato from "./components/Contato";
+import Produto from "./components/Produto";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <div className="content">
           <Routes>
             <Route path="/" element={<Produtos />} />
+            <Route path="produto/:id" element={<Produto />} />
             <Route path="contato" element={<Contato />} />
           </Routes>
         </div>
--- src/components/Produtos.jsx
+++ src/components/Produtos.jsx
@@ -1,11 +1,41 @@
 import React from "react";
 import styles from "../assets/styles/Produtos.module.css";
+import { Link } from "react-router-dom";
+import Head from "./Head";
 
 const Produtos = () => {
+  const [produtos, setProdutos] = React.useState(null);
+  const [error, setError] = React.useState(null);
+
+  React.useEffect(() => {
+    async function fecthProdutos(url) {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setProdutos(json);
+      } catch (erro) {
+        setError("Um erro ocorreu.");
+      }
+    }
+    fecthProdutos("https://ranekapi.origamid.dev/json/api/produto");
+  }, []);
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+  if (produtos === null) {
+    return null;
+  }
   return (
-    <div className={styles.produtos}>
-      <h1>Produtos</h1>
-    </div>
+    <section className={`${styles.produtos} animeLeft`}>
+      <Head title="Ranek" description="Descrição do site Ranek" />
+      {produtos.map((produto) => (
+        <Link to={`produto/${produto.id}`} key={produto.id}>
+          <img src={produto.fotos[0].src} alt={produto.fotos[0].titulo} />
+          <h1 className={styles.nome}>{produto.nome}</h1>
+        </Link>
+      ))}
+    </section>
   );
 };
 
